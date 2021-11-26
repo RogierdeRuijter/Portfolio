0bdf7f87a3063315d5e4c8de570d4662d8050f13
Circle Ci <rogierderuijter@hotmail.com>
2021-11-26 22:09:59 +0100

    fix prettier

--- src/projects/CircleGame/CircleGame.js
+++ src/projects/CircleGame/CircleGame.js
@@ -6,20 +6,18 @@ import Waveform from "../../helpers/Waveform/Waveform";
 
 function CircleGame() {
   return (
-
-    
-      <ContentWithAudioContainer id="circle-game-container" title={"Circle game"}>
-        <iframe
-          id="circle-game-iframe"
-          title="circle-game"
-          className="iframe-size"
-          src="https://circle-ga.me"
-          loading="lazy"
-          frameBorder="0"
-          style={{ borderRadius: "10px" }}
-        ></iframe>
-        <Waveform project="circle-game" />
-      </ContentWithAudioContainer>
+    <ContentWithAudioContainer id="circle-game-container" title={"Circle game"}>
+      <iframe
+        id="circle-game-iframe"
+        title="circle-game"
+        className="iframe-size"
+        src="https://circle-ga.me"
+        loading="lazy"
+        frameBorder="0"
+        style={{ borderRadius: "10px" }}
+      ></iframe>
+      <Waveform project="circle-game" />
+    </ContentWithAudioContainer>
   );
 }
 
