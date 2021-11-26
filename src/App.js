9f79a62bb85931e7dc302f73d6238f8945315885
Circle Ci <rogierderuijter@hotmail.com>
2021-11-26 21:53:17 +0100

    clean up app.js

--- src/App.js
+++ src/App.js
@@ -12,28 +12,14 @@ const App = () => {
   return (
     <div id="content-container">
       <div id="project-content">
-        <Welcome></Welcome>
-        <div className="empty-spacing">
-          <FishRace />
-        </div>
-        <div className="empty-spacing">
-          <CircleGame />
-        </div>
-        <div className="empty-spacing">
-          <Montepoeli />
-        </div>
-        <div className="empty-spacing">
-          <GetLow />
-        </div>
-        <div className="empty-spacing">
-          <SleepingTumblrSeals />
-        </div>
-        <div className="empty-spacing">
-          <Julia />
-        </div>
-        <div className="empty-spacing">
-          <Info />
-        </div>
+        <Welcome />
+        <FishRace />
+        <CircleGame />
+        <Montepoeli />
+        <GetLow />
+        <SleepingTumblrSeals />
+        <Julia />
+        <Info />
       </div>
     </div>
   );
