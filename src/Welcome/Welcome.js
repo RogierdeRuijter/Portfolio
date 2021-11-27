a3faef27cbdcab21a851c8c04d97824903740672
RogierdeRuijter <rogierderuijter@hotmail.com>
2021-11-27 18:25:06 +0100

    Feature/placeholder (#22)
    * add loaderplace holder
    
    * finish off loader
    
    * set scroll-down-element based on safe-are-inset-bottom

--- src/Welcome/Welcome.js
+++ src/Welcome/Welcome.js
@@ -1,13 +1,27 @@
 import "./Welcome.css";
 import film from "../assets/Film.mp4";
+import { useEffect, useState } from "react";
 
 function Welcome() {
+  const [showLoader, setShowLoader] = useState(true);
+  useEffect(() => {
+    var vid = document.getElementById("myVideo");
+    vid.oncanplay = () => {
+      setShowLoader(false);
+    };
+  }, []);
+
   return (
     <div id="welcome-container">
       <div id="my-introduction">Rogier de Ruijter</div>
       <div>
-        <div id="video-of-me">
-          <video autoPlay playsInline muted loop width={600}>
+        <div className="video-container">
+          {showLoader && (
+            <div id={"video-loader-container"}>
+              <div className={"video-loader"}></div>
+            </div>
+          )}
+          <video autoPlay playsInline muted loop width={600} id={"myVideo"}>
             <source src={film} type="video/mp4" />
           </video>
         </div>
