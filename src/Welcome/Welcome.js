04fc40660524b222ac42ded675bc6a610dae8d9f
Circle Ci <rogierderuijter@hotmail.com>
2021-11-28 11:03:41 +0100

    calculate screensize for mobile

--- src/Welcome/Welcome.js
+++ src/Welcome/Welcome.js
@@ -9,6 +9,10 @@ function Welcome() {
     vid.oncanplay = () => {
       setShowLoader(false);
     };
+    // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+    let vh = window.innerHeight * 0.01;
+    // Then we set the value in the --vh custom property to the root of the document
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
   }, []);
 
   return (
