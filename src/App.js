84d141bff4b3f4a3bd97e8a96e6117b973011f15
RogierdeRuijter <rogierderuijter@hotmail.com>
2021-11-28 14:19:52 +0100

    feat: add info icon (#26)
    * feat: add info icon
    
    * clean up
    
    * refactor: method name
    
    * fix typo
    
    * chore: use 16px
    
    * add div around email

--- src/App.js
+++ src/App.js
@@ -7,9 +7,11 @@ import Montepoeli from "./projects/Montepoeli/Montepoeli";
 import Info from "./projects/Info/Info";
 import Julia from "./projects/Julia/Julia";
 import Welcome from "./Welcome/Welcome";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const App = () => {
+  const [showContactInfo, setShowContactInfo] = useState(false);
+
   useEffect(() => {
     // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
     let vh = window.innerHeight * 0.01;
@@ -17,9 +19,39 @@ const App = () => {
     document.documentElement.style.setProperty("--vh", `${vh}px`);
   }, []);
 
+  useEffect(() => {
+    const outsideContactInfoClickHandler = (e) => {
+      if (
+        document.getElementById("contact-info") &&
+        !document.getElementById("contact-info").contains(e.target)
+      ) {
+        if (showContactInfo) {
+          setShowContactInfo(false);
+        }
+      }
+    };
+
+    window.addEventListener("click", outsideContactInfoClickHandler);
+
+    return () => {
+      window.removeEventListener("click", outsideContactInfoClickHandler);
+    };
+  }, [showContactInfo]);
+
   return (
     <div id="content-container">
       <div id="project-content">
+        <div
+          id="info-icon"
+          onClick={() => setShowContactInfo(!showContactInfo)}
+        >
+          ℹ️
+        </div>
+        {showContactInfo && (
+          <div id="contact-info">
+            📧 <div>rogierderuijter@hotmail.com</div>
+          </div>
+        )}
         <Welcome />
         <FishRace />
         <CircleGame />
