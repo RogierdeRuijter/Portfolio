347718675810343d6a80caaed5eb9e2ce6d9facd
Circle Ci <rogierderuijter@hotmail.com>
2021-11-28 12:30:21 +0100

    fix calculation

--- src/App.js
+++ src/App.js
@@ -11,18 +11,10 @@ import { useEffect } from "react";
 
 const App = () => {
   useEffect(() => {
-    const handleResize = () => {
-      // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-      let vh = window.innerHeight * 0.01;
-      // Then we set the value in the --vh custom property to the root of the document
-      document.documentElement.style.setProperty("--vh", `${vh}px`);
-    };
-
-    window.addEventListener("resize", handleResize);
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
+    // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+    let vh = window.innerHeight * 0.01;
+    // Then we set the value in the --vh custom property to the root of the document
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
   }, []);
 
   return (
--- src/Welcome/Welcome.js
+++ src/Welcome/Welcome.js
@@ -1,29 +1,19 @@
 import "./Welcome.css";
 import film from "../assets/Film.mp4";
-import { useEffect } from "react";
 
-function Welcome() {
-  useEffect(() => {
-    var vid = document.getElementById("myVideo");
-    vid.oncanplay = () => {
-      setShowLoader(false);
-    };
-  }, []);
-
-  return (
-    <div id="welcome-container">
-      <div id="my-introduction">Rogier de Ruijter</div>
-      <div>
-        <video autoPlay playsInline muted loop width={600} id={"myVideo"}>
-          <source src={film} type="video/mp4" />
-        </video>
-      </div>
-      <div id="scroll-down-element">
-        <span id="scroll-down-element-line"></span>
-        <span id="scroll-down-element-text">Scroll</span>
-      </div>
+const Welcome = () => (
+  <div id="welcome-container">
+    <div id="my-introduction">Rogier de Ruijter</div>
+    <div>
+      <video autoPlay playsInline muted loop width={600} id={"myVideo"}>
+        <source src={film} type="video/mp4" />
+      </video>
     </div>
-  );
-}
+    <div id="scroll-down-element">
+      <span id="scroll-down-element-line"></span>
+      <span id="scroll-down-element-text">Scroll</span>
+    </div>
+  </div>
+);
 
 export default Welcome;
