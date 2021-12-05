4c79954e6c4fe8d9afefd53436834e1ebfc8bc62
Circle Ci <rogierderuijter@hotmail.com>
2021-12-06 00:06:28 +0100

    remove prop

--- src/helpers/ContentWithAudioContainer/ContentWithAudioContainer.js
+++ src/helpers/ContentWithAudioContainer/ContentWithAudioContainer.js
@@ -1,24 +1,12 @@
 import "./ContentWithAudioContainer.css";
-// TODO: check if noFixedHeightContent prop can be removed. Maybe used by Julia
-function ContentWithAudioContainer({
-  id,
-  noFixedHeightContent,
-  title,
-  children,
-}) {
+
+function ContentWithAudioContainer({ id, title, children }) {
   return (
     <>
       <div className="project-introduction">
         <div>{title}</div>
       </div>
-      <div
-        id={id}
-        className="content-container-with-description"
-        style={{
-          minHeight: noFixedHeightContent ? "" : "100vh",
-          gridTemplateRows: noFixedHeightContent ? "auto 20%" : "80% 20%",
-        }}
-      >
+      <div id={id} className="content-container-with-description">
         {children}
       </div>
     </>
