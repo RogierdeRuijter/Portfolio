c410aa5e65e088d900a223547caeba0e369bac7a
Circle Ci <rogierderuijter@hotmail.com>
2021-12-06 13:10:25 +0100

    feat: remove load video in img tag welcome container

--- src/Welcome/Welcome.js
+++ src/Welcome/Welcome.js
@@ -1,40 +1,19 @@
 import "./Welcome.css";
 // TODO: add optimized image
 import film from "../assets/Film.mp4";
-import { useState } from "react";
 
-const Welcome = () => {
-  const [useVideoTag, setUseVideoTag] = useState(false);
-
-  return (
-    <div id="welcome-container">
-      <div id="my-introduction">Rogier de Ruijter</div>
-      <div>
-        {!useVideoTag && (
-          <img
-            src={film}
-            className="myVideo myVideoImage"
-            onError={() => setUseVideoTag(true)}
-          />
-        )}
-        {useVideoTag && (
-          <video
-            className="myVideo"
-            autoPlay
-            playsInline
-            muted
-            loop
-            width={600}
-          >
-            <source src={film} type="video/mp4" />
-          </video>
-        )}
-      </div>
-      <div id="scroll-down-element">
-        <span id="scroll-down-element-line"></span>
-      </div>
+const Welcome = () => (
+  <div id="welcome-container">
+    <div id="my-introduction">Rogier de Ruijter</div>
+    <div>
+      <video className="myVideo" autoPlay playsInline muted loop width={600}>
+        <source src={film} type="video/mp4" />
+      </video>
     </div>
-  );
-};
+    <div id="scroll-down-element">
+      <span id="scroll-down-element-line"></span>
+    </div>
+  </div>
+);
 
 export default Welcome;
