f416f8cba494788786bfe61d7f932bc3e708e268
Circle Ci <rogierderuijter@hotmail.com>
2021-12-06 13:32:01 +0100

    refactor: move my-website out of react

--- src/App.js
+++ src/App.js
@@ -6,19 +6,11 @@ import GetLow from "./projects/GetLow/GetLow";
 import Montepoeli from "./projects/Montepoeli/Montepoeli";
 import Acknowledgements from "./Acknowledgements/Acknowledgements";
 import Julia from "./projects/Julia/Julia";
-import Welcome from "./Welcome/Welcome";
 import { useEffect, useState } from "react";
 import Info from "./Info/Info";
 
 const App = () => {
   const [showContactInfo, setShowContactInfo] = useState(false);
-
-  useEffect(() => {
-    // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-    let vh = window.innerHeight * 0.01;
-    // Then we set the value in the --vh custom property to the root of the document
-    document.documentElement.style.setProperty("--vh", `${vh}px`);
-  }, []);
 
   useEffect(() => {
     const outsideContactInfoClickHandler = (e) => {
@@ -53,7 +45,6 @@ const App = () => {
             📧 <div>rogierderuijter@hotmail.com</div>
           </div>
         )}
-        <Welcome />
         <FishRace />
         <CircleGame />
         <Montepoeli />
--- src/Welcome/Welcome.js
+++ src/Welcome/Welcome.js
@@ -1,19 +1,11 @@
-import "./Welcome.css";
+// TODO: remove file
+
+// import "./Welcome.css";
 // TODO: add optimized image
-import film from "../assets/Film.mp4";
+// import film from "../assets/Film.mp4";
 
-const Welcome = () => (
-  <div id="welcome-container">
-    <div id="my-introduction">Rogier de Ruijter</div>
-    <div>
-      <video className="myVideo" autoPlay playsInline muted loop width={600}>
-        <source src={film} type="video/mp4" />
-      </video>
-    </div>
-    <div id="scroll-down-element">
-      <span id="scroll-down-element-line"></span>
-    </div>
-  </div>
-);
+// const Welcome = () => (
 
-export default Welcome;
+// );
+
+// export default Welcome;
