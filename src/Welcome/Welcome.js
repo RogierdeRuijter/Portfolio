3afcf4278d3513ed574655003cb45d2b2738282d
Circle Ci <rogierderuijter@hotmail.com>
2021-12-06 01:07:47 +0100

    add hero image in video tag

--- src/Welcome/Welcome.js
+++ src/Welcome/Welcome.js
@@ -1,18 +1,53 @@
 import "./Welcome.css";
 import film from "../assets/Film.mp4";
+import { useState } from "react";
 
-const Welcome = () => (
-  <div id="welcome-container">
-    <div id="my-introduction">Rogier de Ruijter</div>
-    <div>
-      <video className="myVideo" autoPlay playsInline muted loop width={600}>
-        <source src={film} type="video/mp4" />
-      </video>
+const Welcome = () => {
+  const [useVideoTag, setUseVideoTag] = useState(false);
+
+  return (
+    <div id="welcome-container">
+      <div id="my-introduction">Rogier de Ruijter</div>
+      <div>
+        {!useVideoTag && (
+          <img
+            src={film}
+            className="myVideo myVideoImage"
+            onError={() => setUseVideoTag(true)}
+          />
+        )}
+        {useVideoTag && (
+          <video
+            className="myVideo"
+            autoPlay
+            playsInline
+            muted
+            loop
+            width={600}
+          >
+            <source src={film} type="video/mp4" />
+          </video>
+        )}
+      </div>
+      <div id="scroll-down-element">
+        <span id="scroll-down-element-line"></span>
+      </div>
     </div>
-    <div id="scroll-down-element">
-      <span id="scroll-down-element-line"></span>
-    </div>
-  </div>
-);
+  );
+};
 
 export default Welcome;
+
+// {useVideoTag && (
+//   <video
+//     playsInline
+//     muted
+//     controls
+//     width={462}
+//     preload="auto"
+//     className="montepoeli-video"
+//   >
+//     {/* TODO: add additional video qualites */}
+//     <source src={montepoeliVideo} type="video/mp4" />
+//   </video>
+// )}
