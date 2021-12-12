da2de358dae0a18a10f60edf644f9b6a14e58310
RogierdeRuijter <rogierderuijter@hotmail.com>
2021-12-12 22:47:28 +0100

    remove css grid; (#47)

--- src/Acknowledgements/Acknowledgements.js
+++ src/Acknowledgements/Acknowledgements.js
@@ -1,42 +1,36 @@
 import "./Acknowledgements.css";
-import ContentContainer from "../helpers/ContentContainer/ContentContainer";
 
 function Acknowledgements() {
   return (
     <>
-      <div className="content-wrapper"></div>
-      <ContentContainer>
-        <div className="acknowledgements-container">
+      <div className="acknowledgements-container">
+        <div className="acknowledgements-content">
           <div>
-            <div className="acknowledgements-content">
-              <div>
-                I am forever grateful for everyone I have worked with. Thank you
-                so much.
-              </div>
-              <div style={{ marginTop: "16px" }}>
-                Special thanks:
-                <ul className="special-thanks">
-                  <li>
-                    Lourens, your love for development radiates. It touched my
-                    soul in many ways I can't describe. Thank you for giving me
-                    a love for updates.
-                  </li>
-                  <li>Menno, for scaring me into making beautiful things.</li>
-                  <li>Niels, for showing me what a review looks like.</li>
-                  <li>Isabelle, for getting lost in conversations with me.</li>
-                </ul>
-              </div>
-              <div style={{ marginTop: "16px" }}>
-                And above all, thanks mom for the love, support, dedication, and
-                drive to give us whatever we needed.
-              </div>
-              <div style={{ marginTop: "16px" }}>
-                You have made me into what I am.
-              </div>
-            </div>
+            I am forever grateful for everyone I have worked with. Thank you so
+            much.
+          </div>
+          <div style={{ marginTop: "16px" }}>
+            Special thanks:
+            <ul className="special-thanks">
+              <li>
+                Lourens, your love for development radiates. It touched my soul
+                in many ways I can't describe. Thank you for giving me a love
+                for updates.
+              </li>
+              <li>Menno, for scaring me into making beautiful things.</li>
+              <li>Niels, for showing me what a review looks like.</li>
+              <li>Isabelle, for getting lost in conversations with me.</li>
+            </ul>
+          </div>
+          <div style={{ marginTop: "16px" }}>
+            And above all, thanks mom for the love, support, dedication, and
+            drive to give us whatever we needed.
+          </div>
+          <div style={{ marginTop: "16px" }}>
+            You have made me into what I am.
           </div>
         </div>
-      </ContentContainer>
+      </div>
     </>
   );
 }
