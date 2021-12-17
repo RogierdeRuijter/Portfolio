04ed5047ac4f6b72efdff43bfc6aa3a2a5fa349d
Circle Ci <rogierderuijter@hotmail.com>
2021-12-17 19:08:56 +0100

    remove floating info icon

--- src/App.js
+++ src/App.js
@@ -6,51 +6,12 @@ import GetLow from "./projects/GetLow/GetLow";
 import Montepoeli from "./projects/Montepoeli/Montepoeli";
 import Acknowledgements from "./Acknowledgements/Acknowledgements";
 import Julia from "./projects/Julia/Julia";
-import { useEffect, useState } from "react";
 import Info from "./Info/Info";
 
 const App = () => {
-  const [showContactInfo, setShowContactInfo] = useState(false);
-
-  useEffect(() => {
-    const outsideContactInfoClickHandler = (e) => {
-      if (
-        document.getElementById("contact-info") &&
-        !document.getElementById("contact-info").contains(e.target)
-      ) {
-        if (showContactInfo) {
-          setShowContactInfo(false);
-        }
-      }
-    };
-
-    window.addEventListener("click", outsideContactInfoClickHandler);
-
-    return () => {
-      window.removeEventListener("click", outsideContactInfoClickHandler);
-    };
-  }, [showContactInfo]);
-
   return (
     <div id="content-container">
       <div id="project-content">
-        <div
-          id="info-icon"
-          onClick={() => setShowContactInfo(!showContactInfo)}
-        >
-          ℹ️
-        </div>
-        {showContactInfo && (
-          <div id="contact-info">
-            <a
-              className="info-element"
-              href="mailto:rogierderuijter@hotmail.com"
-              label="rogierderuijter.com"
-            >
-              📧 Contact me
-            </a>
-          </div>
-        )}
         <FishRace />
         <CircleGame />
         <Montepoeli />
