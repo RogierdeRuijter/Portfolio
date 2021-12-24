fa16f4f3ae5c73cb8cabb3d9901ec48bfed42705
RogierdeRuijter <rogierderuijter@hotmail.com>
2021-12-24 23:57:28 +0100

    feat: add lazy loading polyfill (#62)
    * feat: add lazy loading polyfill
    
    * chore: remove todo

--- src/projects/CircleGame/CircleGame.js
+++ src/projects/CircleGame/CircleGame.js
@@ -1,26 +1,15 @@
 import "./CircleGame.css";
 import ContentWithAudioContainer from "../../helpers/ContentWithAudioContainer/ContentWithAudioContainer";
 import Waveform from "../../helpers/Waveform/Waveform";
-import { useState, useEffect } from "react";
 
 function CircleGame() {
-  const [iframeUrl, setIframeUrl] = useState();
-
-  useEffect(() => {
-    let timerFunc = setTimeout(() => {
-      setIframeUrl("https://rogierderuijter.github.io/circle/");
-    }, 600);
-
-    return () => clearTimeout(timerFunc);
-  }, []);
-
   return (
     <ContentWithAudioContainer id="circle-game-container" title={"Circle game"}>
       <iframe
         id="circle-game-iframe"
         title="circle-game"
-        className="iframe-size"
-        src={iframeUrl}
+        className="iframe-size lazyload"
+        data-src="https://rogierderuijter.github.io/circle/"
         loading="lazy"
         frameBorder="0"
       ></iframe>
--- src/projects/GetLow/GetLow.js
+++ src/projects/GetLow/GetLow.js
@@ -1,26 +1,15 @@
 import "./GetLow.css";
 import ContentWithAudioContainer from "../../helpers/ContentWithAudioContainer/ContentWithAudioContainer";
 import Waveform from "../../helpers/Waveform/Waveform";
-import { useEffect, useState } from "react";
 
 function GetLow() {
-  const [iframeUrl, setIframeUrl] = useState();
-
-  useEffect(() => {
-    let timerFunc = setTimeout(() => {
-      setIframeUrl("https://rogierderuijter.github.io/get-low/");
-    }, 900);
-
-    return () => clearTimeout(timerFunc);
-  }, []);
-
   return (
     <ContentWithAudioContainer id="get-low-container" title="Get low">
       <iframe
         id="get-low-iframe"
         title="get-low"
-        className="iframe-size"
-        src={iframeUrl}
+        className="iframe-size lazyload"
+        data-src="https://rogierderuijter.github.io/get-low/"
         loading="lazy"
         frameBorder="0"
       ></iframe>
