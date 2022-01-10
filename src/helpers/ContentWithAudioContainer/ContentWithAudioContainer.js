7089c940bc2190ca6f6d7fa397db5919f52ecbb9
Circle Ci <rogierderuijter@hotmail.com>
2022-01-10 10:25:57 +0100

    feat: add explanation for fish-race

--- src/helpers/ContentWithAudioContainer/ContentWithAudioContainer.js
+++ src/helpers/ContentWithAudioContainer/ContentWithAudioContainer.js
@@ -4,7 +4,14 @@ function ContentWithAudioContainer({ id, title, children }) {
   return (
     <>
       <div className="project-introduction">
-        <div>{title}</div>
+        <div id="container">
+          <div id="title">{title}</div>
+          <div>
+            Toen ik 7 was zag ik dit spel op konningendag. 4 vissen die tegen
+            elkaar racen. Ik ben het nooit meer vergeten. Nu kan ik het elke dag
+            spelen.
+          </div>
+        </div>
       </div>
       <div id={id} className="content-container-with-description">
         {children}
