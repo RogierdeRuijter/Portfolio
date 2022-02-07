03a7e247922db7b3f2573e54e2c1b804f1564a66
Circle Ci <rogierderuijter@hotmail.com>
2022-02-07 18:29:50 +0100

    feat: change copy fish race

--- src/projects/FishRace/FishRace.js
+++ src/projects/FishRace/FishRace.js
@@ -5,7 +5,7 @@ function FishRace() {
   return (
     <ContentWithAudioContainer
       id="fish-race-container"
-      description="Toen ik 7 was zag ik op Koninginnedag in het Vondelpark een man naast een bak water met 4 vissen staan. In de bak had hij een vissenrace gemaakt. Mensen konden geld inzetten op hun favoriete vis waarna de man een startshot gaf en de vissen het tegen elkaar opnamen door naar de overkant te zwemmen. Dit heeft zo’n indruk op mij gemaakt dat ik deze herinnering nooit meer vergeten ben."
+      description="Toen ik 7 was zag ik op Koninginnedag in het Vondelpark een man naast een bak water met 4 vissen staan. In de bak had hij een vissenrace gemaakt. Dit heeft zo’n indruk op mij gemaakt dat ik deze herinnering nooit meer vergeten ben."
       title="Fish race"
       project="fish-race"
       flexibleContainer
