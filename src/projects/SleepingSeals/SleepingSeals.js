191c2642a5943dba7f73fe753ba847dd6671c3df
RogierdeRuijter <rogierderuijter@hotmail.com>
2022-02-09 20:23:35 +0100

    Update SleepingSeals.js

--- src/projects/SleepingSeals/SleepingSeals.js
+++ src/projects/SleepingSeals/SleepingSeals.js
@@ -6,7 +6,7 @@ function SleepingTumblrSeals() {
     <ContentWithAudioContainer
       id="sleeping-seals-container"
       title="Sad Sleeping Seals"
-      description="Quotes met slapende zeehonden. Omdat het kan."
+      description="Quotes met slapende zeehonden. Omdat het kan. Click voor meer!"
       project="sleeping-tumblr-seals"
     >
       <iframe
