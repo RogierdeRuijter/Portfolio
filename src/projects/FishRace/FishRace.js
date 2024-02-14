13317b87100b65fe3ae59ba89332896922aa64b9
Rogier <r.de.ruijter@vpro.nl>
2024-02-14 21:30:07 +0100

    Add description for fishrace in popover

--- src/projects/FishRace/FishRace.js
+++ src/projects/FishRace/FishRace.js
@@ -9,6 +9,7 @@ function FishRace() {
         id="fish-race-container"
         title="Fish race"
         project="fish-race"
+        popoverTargetId="fish-race-explanation"
       >
         <IFrame
           id="fish-race-iframe"
@@ -16,6 +17,18 @@ function FishRace() {
           src="https://rogierderuijter.github.io/fish-race/"
         />
       </ContentWithAudioContainer>
+      <div popover="auto" id="fish-race-explanation">
+        Toen ik 7 was zag ik op Koninginnedag in het Vondelpark een man naast
+        een bak water met 4 vissen staan. In de bak had hij een vissenrace
+        gemaakt. Dit heeft zo’n indruk op mij gemaakt dat ik het nooit meer
+        vergeten ben.
+        <button
+          popovertarget="fish-race-explanation"
+          popovertargetaction="hide"
+        >
+          sluit
+        </button>
+      </div>
     </div>
   );
 }
