46a92f929cb761edbc2af3a95f9f94221fa43764
Rogier <r.de.ruijter@vpro.nl>
2024-02-16 12:32:43 +0100

    finish up popover for fish race

--- src/projects/FishRace/FishRace.js
+++ src/projects/FishRace/FishRace.js
@@ -18,16 +18,20 @@ function FishRace() {
         />
       </ContentWithAudioContainer>
       <div popover="auto" id="fish-race-explanation">
-        Toen ik 7 was zag ik op Koninginnedag in het Vondelpark een man naast
-        een bak water met 4 vissen staan. In de bak had hij een vissenrace
-        gemaakt. Dit heeft zo’n indruk op mij gemaakt dat ik het nooit meer
-        vergeten ben.
-        <button
-          popovertarget="fish-race-explanation"
-          popovertargetaction="hide"
-        >
-          sluit
-        </button>
+        <div className="popover-wrapper">
+          <p>
+            Toen ik 7 was zag ik op Koninginnedag in het Vondelpark een man
+            naast een bak water met 4 vissen staan. In de bak had hij een
+            vissenrace gemaakt. Dit heeft zo’n indruk op mij gemaakt dat ik het
+            nooit meer vergeten ben.
+          </p>
+          <button
+            popovertarget="fish-race-explanation"
+            popovertargetaction="hide"
+          >
+            X
+          </button>
+        </div>
       </div>
     </div>
   );
