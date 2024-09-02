ab9eb5f5e7b73b43b5b32685bd7d9953172c71d7
Rogier de Ruijter <r.de.ruijter@vpro.nl>
2024-09-02 18:29:17 +0200

    my blog

--- src/App.js
+++ src/App.js
@@ -1,87 +1,44 @@
+import AllProjects from "./AllProjects";
+import { Routes, Route, Outlet, Link } from "react-router-dom";
+import AllBlogPosts from "./AllBlogPosts";
 import "./App.css";
-import CircleGame from "./projects/CircleGame/CircleGame";
-import FishRace from "./projects/FishRace/FishRace";
-import SleepingTumblrSeals from "./projects/SleepingSeals/SleepingSeals";
-import GetLow from "./projects/GetLow/GetLow";
-import Montepoeli from "./projects/Montepoeli/Montepoeli";
-import Acknowledgements from "./Acknowledgements/Acknowledgements";
-import Julia from "./projects/Julia/Julia";
-import Info from "./Info/Info";
-import { useEffect, useLayoutEffect } from "react";
-import DropItem from "./helpers/DropItem/DropItem";
 
 const App = () => {
-  useLayoutEffect(() => {
-    function getResizeMessage(event) {
-      const juliaIframe = document.getElementById("julia-iframe");
-      const iframes = [juliaIframe];
-      iframes.forEach((iframe) => {
-        if (iframe.contentWindow === event.source) {
-          iframe.classList.remove("default-height");
-          const height = Number(event.data.height);
-
-          document
-            .getElementsByTagName("body")[0]
-            .style.setProperty("--julia-iframe-height", `${height}px`);
-          iframe.height = height;
-        }
-      });
-    }
-
-    window.addEventListener("message", getResizeMessage, false);
-
-    return () => {
-      window.removeEventListener("message", getResizeMessage);
-    };
-  }, []);
-
-  useEffect(() => {
-    if ("loading" in HTMLIFrameElement.prototype) {
-      const iframes = document.querySelectorAll('iframe[loading="lazy"]');
-
-      iframes.forEach((iframe) => {
-        iframe.src = iframe.dataset.src;
-      });
-    } else {
-      // Dynamically import the LazySizes library
-      const script = document.createElement("script");
-      script.src =
-        "https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.2.2/lazysizes.min.js";
-      document.body.appendChild(script);
-    }
-
-    const listener = window.addEventListener("blur", () => {
-      const popoverApiSupported =
-        window.HTMLElement.prototype.hasOwnProperty("popover");
-      if (popoverApiSupported) {
-        document.querySelectorAll("[popover]").forEach((popover) => {
-          popover?.hidePopover();
-        });
-      }
-      window.removeEventListener("blur", listener);
-    });
-  }, []);
-
   return (
-    <div id="content-container">
-      <section id="project-content">
-        <DropItem />
-        <FishRace />
-        <DropItem />
-        <CircleGame />
-        <DropItem />
-        <Montepoeli />
-        <DropItem />
-        <GetLow />
-        <DropItem />
-        <SleepingTumblrSeals />
-        <DropItem />
-        <Julia />
-        <Acknowledgements />
-        <Info />
-      </section>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<AllProjects />} />
+          <Route path="blog" element={<AllBlogPosts />} />
+
+          {/* Using path="*"" means "match anything", so this route
+                acts like a catch-all for URLs that we don't have explicit
+                routes for. */}
+          <Route path="*" element={<div />} />
+        </Route>
+      </Routes>
+    </>
   );
 };
+
+function Layout() {
+  return (
+    <>
+      <nav class="nav">
+        <ul class="list">
+          <li class="item">
+            <Link to="/blog">Blog</Link>
+          </li>
+          <li class="item">
+            <Link to="/">Home</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <hr />
+      <Outlet />
+    </>
+  );
+}
 
 export default App;
---
+++ src/SyntaxHighlighting.js
@@ -0,0 +1,69 @@
+import React from "react";
+import ReactMarkdown from "react-markdown";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { tomorrow } from "react-syntax-highlighter/dist/esm/styles/prism";
+import rehypeRaw from "rehype-raw";
+import rehypeSlug from 'rehype-slug';
+
+
+function SyntaxHighlighting({ title, children, date }) {
+  return (
+    <div
+      style={{
+        padding: "0 2rem",
+        maxWidth: "60ch",
+        margin: "auto",
+        lineHeight: 1.6,
+        fontSize: "1.15rem",
+      }}
+    >
+      {date}
+      <ReactMarkdown
+        rehypePlugins={[rehypeRaw, rehypeSlug]}
+        components={{
+          a: ({ node, ...props }) => (
+            <a target="_blank" rel="noopener noreferrer" {...props} />
+          ),
+          h1: ({ node, ...props }) => (
+            <h1 id={props.id || props.children[0].replace(/\s+/g, '-').toLowerCase()} {...props} />
+          ),
+          h2: ({ node, ...props }) => {
+            if (title) {
+              props.id = `${title}~${props.id}`;
+            } else {
+              console.warn('No title found for id generation')
+            }
+
+            return (<h2 id={props.id} {...props} />)
+          },
+          h3: ({ node, ...props }) => {
+            props.id = undefined;
+
+            return (<h3 id={props.id} {...props} />)
+          },
+          code({ node, inline, className, children, ...props }) {
+            const match = /language-(\w+)/.exec(className || "");
+            return !inline && match ? (
+              <SyntaxHighlighter
+                style={tomorrow}
+                language={match[1]}
+                PreTag="div"
+                {...props}
+              >
+                {String(children).replace(/\n$/, "")}
+              </SyntaxHighlighter>
+            ) : (
+              <code className={className} {...props}>
+                {children}
+              </code>
+            );
+          },
+        }}
+      >
+        {children}
+      </ReactMarkdown>
+    </div>
+  );
+}
+
+export default SyntaxHighlighting;
