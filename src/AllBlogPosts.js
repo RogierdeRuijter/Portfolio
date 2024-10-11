0774e9efa34c8915808904a9f088a36dbbd2125b
Rogier de Ruijter <r.de.ruijter@vpro.nl>
2024-10-11 20:33:18 +0200

    add detail caniuse blog post to all blog posts

--- src/AllBlogPosts.js
+++ src/AllBlogPosts.js
@@ -1,10 +1,13 @@
 import FirstPost from "./posts/1-FirstPost";
 import SecondPost from "./posts/2-UseCaseWebcomponent";
+import ThirdPost from "./posts/3-one-detail-can-i-use";
 import "./AllBlogPosts.css";
 
 export default function AllBlogPosts() {
   return (
     <div className="blog-container">
+      <ThirdPost />
+      <hr />
       <SecondPost />
       <hr />
       <FirstPost />
